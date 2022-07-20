21cce52b9258c710ae5eeffb32b6fe0b614caa0d
CMiller <mlr.chase40@gmail.com>
2022-07-20 16:46:25 -0500

    added style sheet

--- src/page-template.js
+++ src/page-template.js
@@ -4,37 +4,51 @@ const displayTeam = team => {
     if (member.getRole() === 'Engineer') {
       teamHTML += `
         <div class="team-member">
-            <h2>${member.getName()}</h2>
-            <h3>${member.getRole()}</h3>
-            <p>ID: ${member.getId()}</p>
-            <p>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
-            <p>GitHub: ${member.getGithub()}</p>
+            <div class="card-title">
+                <h2>${member.getName()}</h2>
+                <h3>${member.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <p>ID: ${member.getId()}</p>
+                <p>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
+                <p>GitHub: <a target="_blank" href="https://github.com/${member.getGithub()}">${member.getGithub()}</a></p>
+            </div>
         </div>
         `;
     } else if (member.getRole() === 'Intern') {
       teamHTML += `
         <div class="team-member">
-            <h2>${member.getName()}</h2>
-            <h3>${member.getRole()}</h3>
-            <p>ID: ${member.getId()}</p>
-            <p>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
-            <p>School: ${member.getSchool()}</p>
+            <div class="card-title">
+                <h2>${member.getName()}</h2>
+                <h3>${member.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <p>ID: ${member.getId()}</p>
+                <p>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
+                <p>School: ${member.getSchool()}</p>
+            </div>
         </div>
         `;
     } else {
       teamHTML += `
         <div class="team-member">
-            <h2>${member.getName()}</h2>
-            <h3>${member.getRole()}</h3>
-            <p>ID :${member.getId()}</p>
-            <p>Email :<a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
-            <p>Office number: ${member.getOfficeNumber()}</p>
+            <div class="card-title">
+                <h2>${member.getName()}</h2>
+                <h3>${member.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <p>ID: ${member.getId()}</p>
+                <p>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></p>
+                <p>Office number: ${member.getOfficeNumber()}</p>
+            </div>
         </div>
         `;
     }
   });
   return teamHTML;
 };
+
+const year = new Date().getFullYear();
 
 module.exports = team => {
   console.log(team);
@@ -56,7 +70,7 @@ module.exports = team => {
                 ${displayTeam(team)}
             </main>
             <footer>
-                <p>&copy; ${team[0].name}</p>
+                <p>&copy; ${team[0].name} ${year}</p>
             </footer>
         </body>
         </html>
